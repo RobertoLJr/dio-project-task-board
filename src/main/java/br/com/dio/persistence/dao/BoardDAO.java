e657bb46993b4365f94a6f664f2cbabe074d464f
Roberto Junior <gsn.roberto@gmail.com>
2025-07-26 12:07:24 -0300

    feat: add Board DAO and Service

---
+++ src/main/java/br/com/dio/persistence/dao/BoardDAO.java
@@ -0,0 +1,64 @@
+package br.com.dio.persistence.dao;
+
+import br.com.dio.persistence.entity.BoardEntity;
+import com.mysql.cj.jdbc.StatementImpl;
+import lombok.AllArgsConstructor;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Optional;
+
+@AllArgsConstructor
+public class BoardDAO {
+    private final Connection connection;
+
+    public BoardEntity create(final BoardEntity board) throws SQLException {
+        var sql = "INSERT INTO boards (name) VALUES (?);";
+
+        try (var statement = connection.prepareStatement(sql)) {
+            statement.setString(1, board.getName());
+            statement.executeUpdate();
+            if (statement instanceof StatementImpl impl) {
+                board.setId(impl.getLastInsertID());
+            }
+        }
+
+        return board;
+    }
+
+    public Optional<BoardEntity> findById(final Long id) throws SQLException {
+        var sql = "SELECT * FROM boards WHERE id = ?;";
+
+        try (var statement = connection.prepareStatement(sql)) {
+            statement.setLong(1, id);
+            statement.executeQuery();
+            var resultSet = statement.getResultSet();
+            if (resultSet.next()) {
+                var board = new BoardEntity();
+                board.setId(resultSet.getLong("id"));
+                board.setName(resultSet.getString("name"));
+                return Optional.of(board);
+            }
+            return Optional.empty();
+        }
+    }
+
+    public void delete(final Long id) throws SQLException {
+        var sql = "DELETE FROM boards WHERE id = ?;";
+
+        try (var statement = connection.prepareStatement(sql)) {
+            statement.setLong(1, id);
+            statement.executeUpdate();
+        }
+    }
+
+    public boolean exists(final Long id) throws SQLException {
+        var sql = "SELECT 1 FROM boards WHERE id = ?;";
+
+        try (var statement = connection.prepareStatement(sql)) {
+            statement.setLong(1, id);
+            statement.executeQuery();
+            return statement.getResultSet().next();
+        }
+    }
+}
